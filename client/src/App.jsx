ed358d8cc60325b1cd248148dec4ca298aa6200c
renokazaki <uvxmwvhva27ejb67dkcr@withone.co.jp>
2024-06-19 17:27:45 +0900

    at 1727

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,55 @@
-import "./App.css";
-import io from "socket.io-client";
-import { useState, useEffect } from "react";
+import Rectagle from "./Rectangle";
 
-const socket = io.connect("https://chat-app-5y9l.onrender.com");
 const App = () => {
-  const [inputMessage, setInputMessage] = useState("");
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    socket.on("new_message", (data) => {
-      setMessage(data.message);
-    });
-  }, [socket]);
-
-  const handleChange = (e) => {
-    setInputMessage(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    socket.emit("send_message", { message: inputMessage });
-    setInputMessage("");
-  };
   return (
     <div>
-      <h1>Chat App</h1>
-
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="inputMessage"
-          value={inputMessage}
-          onChange={handleChange}
-          placeholder="Enter Message"
-        />
-        <button type="submit">Send Message</button>
-      </form>
-
-      {message && <h2> {message} </h2>}
+      <Rectagle />
     </div>
   );
 };
 export default App;
+
+// import "./App.css";
+// import io from "socket.io-client";
+// import { useState, useEffect } from "react";
+
+// const socket = io.connect("https://chat-app-5y9l.onrender.com");
+// const App = () => {
+//   const [inputMessage, setInputMessage] = useState("");
+//   const [message, setMessage] = useState("");
+
+//   useEffect(() => {
+//     socket.on("new_message", (data) => {
+//       setMessage(data.message);
+//     });
+//   }, [socket]);
+
+//   const handleChange = (e) => {
+//     setInputMessage(e.target.value);
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     socket.emit("send_message", { message: inputMessage });
+//     setInputMessage("");
+//   };
+//   return (
+//     <div>
+//       <h1>Chat App</h1>
+
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           type="text"
+//           name="inputMessage"
+//           value={inputMessage}
+//           onChange={handleChange}
+//           placeholder="Enter Message"
+//         />
+//         <button type="submit">Send Message</button>
+//       </form>
+
+//       {message && <h2> {message} </h2>}
+//     </div>
+//   );
+// };
+// export default App;
---
+++ client/src/Rectangle.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
+
+const socket = io.connect("https://chat-app-5y9l.onrender.com");
+const Rectagle = () => {
+  const [inputMessage, setInputMessage] = useState("");
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    socket.on("new_message", (data) => {
+      setMessage(data.message);
+    });
+  }, [socket]);
+
+  const handleChange = (e) => {
+    setInputMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("send_message", { message: inputMessage });
+    setInputMessage("");
+  };
+  return (
+    <div>
+      <h1>Chat App</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="inputMessage"
+          value={inputMessage}
+          onChange={handleChange}
+          placeholder="Enter Message"
+        />
+        <button type="submit">Send Message</button>
+      </form>
+
+      {message && <h2> {message} </h2>}
+    </div>
+  );
+};
+export default Rectagle;
